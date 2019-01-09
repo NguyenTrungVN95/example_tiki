8893824027a7ca0d84ac94f5469baf8c1c3ba27c
NguyenTrungVN95 <trungnm@beetechsoft.com>
2019-01-09 11:40:19 +0700

    update logic for check style item version 2

--- app/src/main/java/nguyentrung/example/com/exam_nguyentrung/presenter/MainPresenter.java
+++ app/src/main/java/nguyentrung/example/com/exam_nguyentrung/presenter/MainPresenter.java
@@ -13,6 +13,24 @@ public class MainPresenter {
     }
 
     public String[] getData() {
+        for (int i = 0; i < keys.getkeywords().length; i++) {
+            String[] arrKey = keys.getkeywords()[i].split(" ");
+            if (arrKey.length == 1) {
+                keys.getkeywords()[i] = arrKey[0];
+            } else if (arrKey.length == 2) {
+                keys.getkeywords()[i] = arrKey[0] + "\n" + arrKey[1];
+            } else if (arrKey.length == 3) {
+                keys.getkeywords()[i] = arrKey[0] + " " + arrKey[1] + "\n" + arrKey[2];
+            } else if (arrKey.length == 4) {
+                keys.getkeywords()[i] = arrKey[0] + " " + arrKey[1] + "\n" + arrKey[2] + " " + arrKey[3];
+            } else if (arrKey.length == 5) {
+                keys.getkeywords()[i] = arrKey[0] + " " + arrKey[1] + "\n" + arrKey[2] + " " + arrKey[3] + " " + arrKey[4];
+            } else if (arrKey.length == 6) {
+                keys.getkeywords()[i] = arrKey[0] + " " + arrKey[1] + " " + arrKey[2] + "\n" + arrKey[3] + " " + arrKey[4] + " " + arrKey[5];
+            } else if (arrKey.length == 7) {
+                keys.getkeywords()[i] = arrKey[0] + " " + arrKey[1] + " " + arrKey[2] + "\n" + arrKey[3] + " " + arrKey[4] + " " + arrKey[5] + " " + arrKey[6];
+            }
+        }
         return keys.getkeywords();
     }
 
--- app/src/main/java/nguyentrung/example/com/exam_nguyentrung/view/RecyclerViewAdapter.java
+++ app/src/main/java/nguyentrung/example/com/exam_nguyentrung/view/RecyclerViewAdapter.java
@@ -3,11 +3,13 @@ package nguyentrung.example.com.exam_nguyentrung.view;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +23,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private String[] mNames = new String[]{"", "", ""};
     private Context mcontext;
     private Random rnd = new Random();
+    static String[] arrKey =new String[]{"", "", ""};
 
     private int color;
 
@@ -39,29 +42,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @SuppressLint("ResourceType")
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        rnd = new Random();
-        color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
 
-        String[] arrKey = mNames[position].split(" ");
-
-        if (arrKey.length == 1) {
-            mNames[position] = arrKey[0];
-        } else if (arrKey.length == 2) {
-        } else if (arrKey.length == 3) {
-            mNames[position] = arrKey[0] + " " + arrKey[1] + "\n" + arrKey[2];
-        } else if (arrKey.length == 4) {
-            mNames[position] = arrKey[0] + " " + arrKey[1] + "\n" + arrKey[2] + " " + arrKey[3];
-        } else if (arrKey.length == 5) {
-            mNames[position] = arrKey[0] + " " + arrKey[1] + "\n" + arrKey[2] + " " + arrKey[3] + " " + arrKey[4];
-        } else if (arrKey.length == 6) {
-            mNames[position] = arrKey[0] + " " + arrKey[1] + " " + arrKey[2] + "\n" + arrKey[3] + " " + arrKey[4] + " " + arrKey[5];
-        } else if (arrKey.length == 7) {
-            mNames[position] = arrKey[0] + " " + arrKey[1] + " " + arrKey[2] + "\n" + arrKey[3] + " " + arrKey[4] + " " + arrKey[5] + " " + arrKey[6];
-        } else {
-            mNames[position] = mNames[position];
-        }
         holder.txt.setText(mNames[position]);
-        holder.txt.setBackgroundColor(color);
         holder.parent_layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,10 +62,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         TextView txt;
         RelativeLayout parent_layout;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
+        public ViewHolder(View v) {
+            super(v);
             txt = itemView.findViewById(R.id.text);
             parent_layout = itemView.findViewById(R.id.parent_layout);
+            rnd = new Random();
+            color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            v.setBackgroundResource(R.drawable.tags_rounded_corners);
+            GradientDrawable drawable = (GradientDrawable) v.getBackground();
+            drawable.setColor(color);
+
+
         }
     }
 }
